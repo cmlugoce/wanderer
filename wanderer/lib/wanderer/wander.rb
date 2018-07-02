f6167dc13bd89bd575cf26f6ae7f40fa22ffffa7
Cristina Lugo <cmlugoce@syr.edu>
2018-07-02 16:37:41 +0000

    changed from national parks to long hikes

--- wanderer/lib/wanderer/wander.rb
+++ wanderer/lib/wanderer/wander.rb
@@ -8,19 +8,20 @@ module Wanderer
 
   class Wander
 
-    attr_accessor :name, :location, :description
+    attr_accessor :name, :location, :description, :distance
 
 
       def self.get_page
 
-           doc = Nokogiri::HTML(open("https://www.fodors.com/news/photos/14-of-the-most-beautiful-seashores-in-americas-national-parks"))
+           doc = Nokogiri::HTML(open("https://www.fodors.com/news/photos/15-amazing-long-distance-hikes-around-the-world"))
 
            trips = doc.css(".container.slides")
-           trips.collect do |park|
+           trips.collect do |hike|
              place = self.new
-             place.name = park.css('h2').text.strip.gsub("Book a Hotel", "").gsub("\n", "").strip
-             place.location = park.css('h3 span').text.strip.gsub("\n", "")
-             place.description = park.css("p").text
+             place.name = hike.css('h2').text.strip.gsub("Book a Hotel", "").gsub("\n", "").strip
+             place.location = hike.css('h3 span').text.strip.gsub("\n", "")
+            place.distance = hike.css("p").children[1].text
+             place.description = hike.css("p").children[2..20].text.strip.gsub("\n", "").strip
 
              place
              end
